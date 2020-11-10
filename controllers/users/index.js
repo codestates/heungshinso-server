ea4cd7547f869107f837c60e24a4389563d4a5bb
godkor200 <67778293+godkor200@users.noreply.github.com>
2020-11-10 20:04:38 +0900

    Basic Routing

---
+++ controllers/users/index.js
@@ -0,0 +1,15 @@
+exports.signup = (req, res) => {
+  res.send("signup");
+};
+
+exports.signin = (req, res) => {
+  res.send("signin");
+};
+
+exports.signout = (req, res) => {
+  res.send("signout");
+};
+
+exports.profile = (req, res) => {
+  res.send("profile");
+};
