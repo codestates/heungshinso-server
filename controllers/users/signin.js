a12673bcd1dc3627ea2bfceeedf242ce39b2cb12
YUBYEONGGUK <godkor200@gmail.com>
2020-11-13 14:45:06 +0900

    Add signin functionality

--- controllers/users/signin.js
+++ controllers/users/signin.js
@@ -1,7 +1,21 @@
-const models = require("../../models");
+const users = require("../../models").user;
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   const { email, password } = req.body;
   const session = req.session;
-  res.send("signin");
+  try {
+    await users
+      .findOne({ where: { email: email, password: password } })
+      .then((user) => {
+        if (!user) {
+          res.status(404).send("해당하는 유저가 없거나 비밀번호가 틀립니다.");
+        } else {
+          session.userid = user.id;
+          res.status(200).send({ id: user.id });
+        }
+      });
+  } catch (error) {
+    res.status(500).send("서버에서 응답이 없습니다.");
+    console.error(error);
+  }
 };
