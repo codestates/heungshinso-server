0379ff386ed60920f03c60d0d0b63ec9710fd0df
YUBYEONGGUK <godkor200@gmail.com>
2020-11-13 15:54:26 +0900

    Implement member information modified function

--- controllers/users/profile.js
+++ controllers/users/profile.js
@@ -1,3 +1,33 @@
-module.exports = (req, res) => {
-  res.send("profile");
+const users = require("../../models").user;
+
+module.exports = async (req, res) => {
+  const session = req.session;
+  const {
+    email,
+    password,
+    username,
+    phone_number,
+    birthday,
+    user_region,
+    user_position,
+    user_status,
+  } = req.body;
+  try {
+    await users
+      .update(
+        {
+          username: username,
+          phone_number: phone_number,
+          birthday: birthday,
+          user_region: user_region,
+          user_position: user_position,
+          user_status: user_status,
+        },
+        { where: { id: session.userId } }
+      )
+      .then(res.status(200).send("등록되었습니다."));
+  } catch (error) {
+    res.status(401).send(err);
+    console.error(error);
+  }
 };
