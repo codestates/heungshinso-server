16cffb4943d8ec98e29f9bd24812fb3b8177ef79
PreAgile <>
2020-11-13 16:52:42 +0900

    fix routes/index.js delete githublogin practice

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const models = require('../models');
-const axios = require('axios');
-const fetch = require('node-fetch');
 require('dotenv').config();
 
 router.get('/', (req, res) => {
-  const requestToken = req.query.code;
-  console.log(req.params);
-  console.log("requestToken:" +requestToken);
-  const clientID = process.env.GITHUB_CLIENT_ID;
-  const clientSecret = process.env.GITHUB_CLIENT_SECRET;
-  let accessToken;
-  if (requestToken) {
-    axios({
-      method: 'post',
-      url: `https://github.com/login/oauth/access_token?client_id=${clientID}&client_secret=${clientSecret}&code=${requestToken}`,
-      headers: {
-        accept: 'application/json',
-      },
-    }).then((response) => {
-      accessToken = response.data.access_token;
-      console.dir("accessToken:"+ accessToken);
-     axios('https://api.github.com/user', {
-       headers : {
-        Authorization: 'token ' + accessToken
-       }
-     })
-        .then((res) => res.json())
-        .then((res) => {
-          // https://developer.github.com/v3/users/#get-the-authenticated-user
-          console.dir(`${res.name}님 환영합니다!`);
-        }).catch((err) => console.log("Error"));
-    })
-  }
-
   models.user
     .findAll()
     .then((userResult) => {
