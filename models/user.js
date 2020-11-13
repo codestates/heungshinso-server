6ea22075d83a762f8a104c7fa69e6ae4c8ac8866
YUBYEONGGUK <godkor200@gmail.com>
2020-11-13 16:10:21 +0900

    add hook function

--- models/user.js
+++ models/user.js
@@ -29,14 +29,23 @@ module.exports = (sequelize, DataTypes) => {
     }
   );
   //hook
-  //회원가입할때
+  //회원가입할때시 비밀번호 해쉬
   user.addHook("beforeCreate", (data) => {
     let salt = "random string";
     let shasum = crypto.createHash("sha1");
     shasum.update(data.password + salt);
     data.password = shasum.digest("hex");
   });
-  //로그인할때
+  //회원정보수정시 비밀번호 해쉬
+  user.addHook("afterUpdate", (data) => {
+    let salt = "random string";
+    if (data.where.password) {
+      let shasum = crypto.createHash("sha1");
+      shasum.update(data.where.password + salt);
+      data.where.password = shasum.digest("hex");
+    }
+  });
+  //로그인할때시 비밀번호 해쉬
   user.addHook("beforeFind", (data) => {
     let salt = "random string";
     if (data.where.password) {
