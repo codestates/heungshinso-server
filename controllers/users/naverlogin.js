753c061a52017c9138e064c92dd2e4945a8a5ceb
PreAgile <>
2020-11-14 19:57:58 +0900

    naver login oauth setting

--- controllers/users/naverlogin.js
+++ controllers/users/naverlogin.js
@@ -1,32 +1,56 @@
 require('dotenv').config();
-const client_id = process.env.NAVER_CLIENT_ID//개발자센터에서 발급받은 Client ID
-const client_secret = process.env.NAVER_CLIENT_SECRET //개발자센터에서 발급받은 Client Secret
-let state = "12345"; // random 문자열 
-const redirectURI = encodeURI("https://0cbff0efed25.ngrok.io/users/signin/naverlogin/callback");
-let api_url = "";
+const client_id = process.env.NAVER_CLIENT_ID; //개발자센터에서 발급받은 Client ID
+const client_secret = process.env.NAVER_CLIENT_SECRET; //개발자센터에서 발급받은 Client Secret
+let state = '12345'; // random 문자열
+const redirectURI = encodeURI(
+  'https://fc95d3511f1f.ngrok.io/users/signin/naverlogin/callback'
+);
+let api_url = '';
 module.exports = {
   get: (req, res) => {
-    api_url = 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=' + client_id + '&redirect_uri=' + redirectURI + '&state=' + state;
-    res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
-    res.end("<a href='"+ api_url + "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>");
+    api_url =
+      'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=' +
+      client_id +
+      '&redirect_uri=' +
+      redirectURI +
+      '&state=' +
+      state;
+    res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
+    res.end(
+      "<a href='" +
+        api_url +
+        "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>"
+    );
   },
   getcallback: (req, res) => {
     code = req.query.code;
     state = req.query.state;
-    api_url = 'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id='
-     + client_id + '&client_secret=' + client_secret + '&redirect_uri=' + redirectURI + '&code=' + code + '&state=' + state;
+    api_url =
+      'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=' +
+      client_id +
+      '&client_secret=' +
+      client_secret +
+      '&redirect_uri=' +
+      redirectURI +
+      '&code=' +
+      code +
+      '&state=' +
+      state;
     const request = require('request');
     const options = {
-        url: api_url,
-        headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
-     };
+      url: api_url,
+      headers: {
+        'X-Naver-Client-Id': client_id,
+        'X-Naver-Client-Secret': client_secret,
+      },
+    };
     request.get(options, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        res.redirect(`https://0cbff0efed25.ngrok.io/?access_token=${body}`);
+        res.redirect(`https://fc95d3511f1f.ngrok.io/?access_token=${body}`);
       } else {
         res.status(response.statusCode).end();
         console.log('error = ' + response.statusCode);
       }
     });
-  }
-}
+  },
+};
