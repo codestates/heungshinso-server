820ee17d4fcf198e03587d78e38ce5e636bef0b6
PreAgile <>
2020-11-15 16:15:57 +0900

    fix port and rollback server -> http

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const logger = require("morgan");
 const app = express();
-const fs = require('fs');
-const http = require('http');
-const https = require('https');
-const httpsPort = 3000;
-const httpPort = 3001;
+// const fs = require('fs');
+// const http = require('http');
+// const https = require('https');
+const port = 3000;
+// const httpPort = 3001;
 const cors = require("cors");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
@@ -15,12 +15,12 @@ const teamRouter = require("./routes/team");
 const userRouter = require("./routes/users");
 
 // ssl 인증서 
-const key = fs.readFileSync(__dirname + '/selfsigned.key');
-const cert = fs.readFileSync(__dirname + '/selfsigned.crt');
-const options = {
-  key: key,
-  cert: cert
-};
+// const key = fs.readFileSync(__dirname + '/selfsigned.key');
+// const cert = fs.readFileSync(__dirname + '/selfsigned.crt');
+// const options = {
+//   key: key,
+//   cert: cert
+// };
 
 //db
 const sequelize = require("./models/index").sequelize;
@@ -53,20 +53,20 @@ app.use("/team", teamRouter);
 app.use("/users", userRouter);
 
 
-// app.listen(port, () => {
-//   console.log(`server listening on ${port}`);
+app.listen(port, () => {
+  console.log(`server listening on ${port}`);
+});
+
+// const httpsServer = https.createServer(options, app);
+// const httpServer = http.createServer(app);
+
+// httpsServer.listen(httpsPort, () => {
+//   console.log("httpsServer starting on port : " + httpsPort)
 // });
 
-const httpsServer = https.createServer(options, app);
-const httpServer = http.createServer(app);
-
-httpsServer.listen(httpsPort, () => {
-  console.log("httpsServer starting on port : " + httpsPort)
-});
-
-httpServer.listen(httpPort, () => {
-  console.log("httpServer starting on port : " + httpPort)
-});
+// httpServer.listen(httpPort, () => {
+//   console.log("httpServer starting on port : " + httpPort)
+// });
 
 
 module.exports = app;
