eb1c142b91ee405330853a9113556645cbbdafc3
YUBYEONGGUK <godkor200@gmail.com>
2020-11-16 19:07:41 +0900

    get arranged

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/users/index");
-const callback = require('../controllers/users/callback');
-const naver = require('../controllers/users/naverlogin');
-const kakao = require('../controllers/users/kakaologin');
+const api = require("../controllers/api/index");
+const callback = require("../controllers/users/callback");
+const naver = require("../controllers/users/naverlogin");
+const kakao = require("../controllers/users/kakaologin");
 
 router.post("/signup", controller.signup);
 router.post("/signin", controller.signin);
 router.post("/signout", controller.signout);
 router.post("/profile", controller.profile);
+router.get("/apply", api.apply);
+
+// Oauth
+
 router.get("/signin/callback", callback.get);
 router.get("/signin/naverlogin", naver.get);
 router.get("/signin/naverlogin/callback", naver.callback);
